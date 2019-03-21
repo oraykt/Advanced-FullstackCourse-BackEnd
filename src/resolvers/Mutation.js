44a37a17aacbe09f50dce3658e84aa510b852ac2
OrayKT <oray.kt@gmail.com>
2019-03-21 01:26:43 +0100

    prisma admin panel fixed

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,12 +1,15 @@
 const Mutations = {
-    async createItem(parent, args, context, info) {
+    async createItem(parent, args, ctx, info) {
         // TODO: Check if they are logged in
 
-        const item = await context.db.mutation.createItem({
-            data: {
-                ...args
-            }
-        }, info);
+        const item = await ctx.db.mutation.createItem(
+            {
+                data: {
+                    ...args
+                }
+            },
+            info
+        );
         return item;
     }
 };
